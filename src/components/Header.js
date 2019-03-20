6c42016d2cbffe97238a2dd2e11a911ea0a6d672
github username <github email address>
2019-03-20 11:55:29 -0400

    Done.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
       <Fragment>
-        <h1 className="text-center">Welcome to Access Lab's Pizzeria</h1>
+        <h1 className="text-center">Welcome to Access Labs Pizzeria</h1>
       </Fragment>
     );
   }
